4bf0ec22dae68edcefa07ead0a58f4bfc4040066
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-07-01 17:55:16 -0300

    alt link css

--- src/Components/Links/index.js
+++ src/Components/Links/index.js
@@ -2,10 +2,10 @@ import styles from './Links.css'
 
 export default function Links() {
     return (
-        <div className="d-flex flex-column justfy-content-center align-items-center">
+        <section className="d-flex flex-column justfy-content-center align-items-center link-container">
             <a href="/" className="nav-link btn btn-lg btn-link">Agendamentos</a>
             <a href="/" className="nav-link btn btn-lg btn-link">Instagram</a>
             <a href="/" className="nav-link btn btn-lg btn-link">Youtube</a>
-        </div>
+        </section>
     )
 }
