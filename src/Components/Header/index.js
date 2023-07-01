67ae452d4dd6655efb4b707db3ac05f236c7dedb
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-07-01 17:56:07 -0300

    alt tag

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,9 @@
 import styles from './Header.css'
 export default function Header () {
     return (
-        <div className='d-flex justify-content-center'>
+        <section className='d-flex justify-content-center'>
             <span className="txt-header">dr. pedro assunção</span>
 
-        </div>
+        </section>
     )
 }
