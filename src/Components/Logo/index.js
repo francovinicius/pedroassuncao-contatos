a5bd2ed3387f7d372672ac7a72ecb7c9b7744e9d
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-07-01 18:34:57 -0300

    alt img: width

--- src/Components/Logo/index.js
+++ src/Components/Logo/index.js
@@ -1,7 +1,7 @@
 import styles from './Logo.css'
 export default function Logo() {
     return (
-        <div>
+        <div className='d-flex justify-content-center'>
             <img src='../assets/Logo/logo.png' className="img-logo img-fluid"/>
         </div>
     )
