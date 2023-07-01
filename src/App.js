9b30d4c916d7e0da7534732e51d87a7426b77fee
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-30 23:36:42 -0300

    add img teste

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Background from "./Components/Background";
 
 function App() {
   return (
     <div className="App">
-Funcionou
+      <Background />
     </div>
   );
 }
---
+++ src/Components/Background/index.js
@@ -0,0 +1,5 @@
+export default function Background () {
+    return (
+        <img src="./assets/Background/bg-mobile.png" className="img-fluid"/>
+    )
+}
